532969261c3cc7cf112609d49bced62bdbb278f3
Kapelanq <pixel131513@gmail.com>
2024-09-29 00:50:50 +0200

    options button
    now works
    - fades in
    - fades out

---
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    private bool optionsOpen = false;
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] CanvasGroup uiElement;
+   public void StartGame()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void OpenCloseOptions()
+    {
+        if(optionsOpen)
+        {
+            StartCoroutine(FadeOut());
+            optionsOpen = false;
+        }
+        else
+        {
+            FadeIn();
+            optionsOpen = true;
+        }
+    }
+
+    void FadeIn()
+    {
+        optionsMenu.SetActive(true);
+        LeanTween.alphaCanvas(uiElement, 1f, 1f).setEase(LeanTweenType.linear);
+    }
+    IEnumerator FadeOut()
+    {
+       
+        LeanTween.alphaCanvas(uiElement, 0f, 1f).setEase(LeanTweenType.linear);
+        yield return new WaitForSeconds(1);
+        optionsMenu.SetActive(false);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

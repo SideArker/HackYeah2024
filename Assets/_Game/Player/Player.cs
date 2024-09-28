897376635f49776ef8824c4af9b56e2a16c4da67
SideArker <rsowa126@gmail.com>
2024-09-28 15:56:54 +0200

    Enemy & player behaviour

--- Assets/_Game/Player/Player.cs
+++ Assets/_Game/Player/Player.cs
@@ -4,12 +4,16 @@ using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
 
     public static Player instance;
 
+    [SerializeField] float health = 4;
+
+    public UnityEvent onPlrDeath;
 
     float horizontal;
     float vertical;
@@ -20,18 +24,29 @@ public class Player : MonoBehaviour
     private void Awake()
     {
 
-        if(instance != null)
+        if(instance == null)
         instance = this;
 
         rb = GetComponent<Rigidbody2D>();
     }
 
 
+    public void DamagePlayer()
+    {
+        health--;
+
+        if(health <= 0)
+        {
+            onPlrDeath.Invoke();
+        }
+
+        Debug.Log("Plr dmaged");
+    }
+
     private void Update()
     {
 
         float speed = 4;
-        Debug.Log(speed);
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
 

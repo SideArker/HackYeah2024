c3bb8cac43ca23091f41015e24f92dcf485f4c47
Kapelanq <pixel131513@gmail.com>
2024-09-29 10:41:54 +0200

    build

--- Assets/_Game/Player/Player.cs
+++ Assets/_Game/Player/Player.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Build;
+
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
